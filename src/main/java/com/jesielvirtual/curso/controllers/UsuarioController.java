ba19fc91f7b87150ba2980b96ac208ea3107cc8e
Jesiel Palacios <jopalacioss@unac.edu.co>
2023-09-07 18:45:56 -0500

    Minor changes

--- src/main/java/com/jesielvirtual/curso/controllers/UsuarioController.java
+++ src/main/java/com/jesielvirtual/curso/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.jesielvirtual.curso.controllers;
 
+import com.jesielvirtual.curso.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,16 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public List<String> prueba() {
-        return List.of("Manzana", "Kiwi", "Banana");
+    @RequestMapping(value = "usuario")
+    public Usuario getUser() {
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("Pepito");
+        usuario.setApellido("Perez");
+        usuario.setEmail("pepito.perez@gmail.com");
+        usuario.setTelefono("1234567890");
+        usuario.setPassword("asdasdasd");
+
+        return usuario;
     }
 }
