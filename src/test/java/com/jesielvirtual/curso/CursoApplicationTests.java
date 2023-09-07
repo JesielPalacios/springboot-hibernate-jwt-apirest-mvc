747f6ee89a112f82293a29dea800b7c916cc1bed
Jesiel Palacios <jopalacioss@unac.edu.co>
2023-09-07 14:38:58 -0500

    Initial commit

---
+++ src/test/java/com/jesielvirtual/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesielvirtual.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
